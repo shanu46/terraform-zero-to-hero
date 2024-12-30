c79d1784b1b57e64d4def21e7e97914cdd8b2d58
shanu <67824329+shanu46@users.noreply.github.com>
2024-12-30 18:01:45 +0530

    Add files via upload

---
+++ Day-4/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_id      = "ami-053b12d3152c0cc71"
+bucket_name = "shanu-s3-demo-xyz"
---
+++ Day-4/variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  description = "value of AMI Id"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "value of S3 bucket"
+  type        = string
+}
+
+variable "access_key" {
+  description = "value of access key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "value of secret key"
+  type        = string
+}
